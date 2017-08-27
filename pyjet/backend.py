298c45d72cdcfc74291e1ca193538aaa0cbd5a68
Unknown <aam14@rice.edu>
2017-08-27 14:48:17 -0500

    Moved backend into file
    Backend used to be in a folder of its own. Since the contents were not very large, I moved it to a folder of its own.

--- pyjet/backend.py
+++ pyjet/backend.py
@@ -5,26 +5,34 @@ import torch.nn.functional as F
 epsilon = 1e-11
 
 # Optimization for casting things to cuda tensors
+
+
 def cudaFloatTensor(x):
     return torch.FloatTensor(x).cuda()
+
 
 def cudaLongTensor(x):
     return torch.LongTensor(x).cuda()
 
+
 def cudaByteTensor(x):
     return torch.ByteTensor(x).cuda()
+
 
 def flatten(x):
     """Flattens along axis 0 (# rows in == # rows out)"""
     return x.view(x.size(0), -1)
 
+
 def softmax(x):
-    normalized_exp = (x - x.max(1)[0].expand(*x.size())).exp() # .clamp(epsilon, 1.)
+    normalized_exp = (x - x.max(1)[0].expand(*x.size())).exp()  # .clamp(epsilon, 1.)
     return normalized_exp / normalized_exp.sum(1).expand(*x.size())
+
 
 def zero_center(x):
     return (x - x.mean().expand(*x.size()))
-    
+
+
 def standardize(x):
     std = (x.pow(2).mean() - x.mean().pow(2)).sqrt()
     return zero_center(x) / std.expand(*x.size()).clamp(min=epsilon)
