3570a8d3c8a7c944b68781ae5edbb47a55d8d15a
abhmul <aam14@rice.edu>
2017-08-28 14:12:23 -0500

    GPU run test fix
    Test would normally fail when run on GPU due to not  being able to cast CUDA directly to numpy. This has been fixed by first casting it to a cpu tensor.

--- tests/backend_test.py
+++ tests/backend_test.py
@@ -7,5 +7,5 @@ def test_flatten_3d_tensor():
     test_array = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
     expected_array = np.arange(2 * 3 * 4).reshape(2, 3 * 4).astype(np.float32)
     test_tensor = J.Tensor(test_array)
-    actual_array = J.flatten(test_tensor).numpy()
+    actual_array = J.flatten(test_tensor).cpu().numpy()
     np.testing.assert_array_equal(actual_array, expected_array)
